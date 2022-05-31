ed6510b447e3edd2b2a73023a4ea24436693a9f4
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-05-31 15:32:48 -0300

    Aumentando a qualidade.

--- src/test/java/util/TestValida.java
+++ src/test/java/util/TestValida.java
@@ -49,12 +49,20 @@ public class TestValida {
     public void testValidaCPFValido4() {
         assertTrue(valida.validaCPF("31626333033"));
     }
+    
+    /**
+     * Testa CPF inválido.
+     */
+    @Test
+    public void testValidaCPFInvalido1() {
+        assertFalse(valida.validaCPF("94622036011"));
+    }
 
     /**
      * Testa CPF com problema na conversão.
      */
     @Test
-    public void testValidaCPFInvalido() {
+    public void testValidaCPFInvalido2() {
         //CPF com problema na conversão
         assertFalse(valida.validaCPF("0065XAB22050"));
     }
